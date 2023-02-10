8ec081f324423b0c47e271cb6f75a8895478e6e0
seunghyun <masterhyun1207@nate.com>
2023-02-10 17:26:33 +0900

    2023-02-10 config.html 연결

---
+++ src/main/java/HyunWeb/com/spring/security/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+/*
+ * package HyunWeb.com.spring.security;
+ * 
+ * import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter;
+ * 
+ * public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+ * 
+ * @Override public void configure(HttpSecurity http) throws Exception{
+ * 
+ * } }
+ */
