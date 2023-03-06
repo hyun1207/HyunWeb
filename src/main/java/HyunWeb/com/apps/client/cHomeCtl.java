e14b42d309659aea263d2491cf586968d6865464
seunghyun <masterhyun1207@nate.com>
2023-03-06 10:22:12 +0900

    2023-03-06 페이지 컨텐츠 연결 완료

--- src/main/java/HyunWeb/com/apps/client/cHomeCtl.java
+++ src/main/java/HyunWeb/com/apps/client/cHomeCtl.java
@@ -23,7 +23,7 @@ public class cHomeCtl {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@RequestMapping(value = {"","/"} , method = {RequestMethod.GET,RequestMethod.POST} , produces = "application/json; charset=UTF-8") 
+	@RequestMapping(value = {"","/","/home"} , method = {RequestMethod.GET,RequestMethod.POST} , produces = "application/json; charset=UTF-8")
 	public String cHome(HttpServletRequest request, HttpServletResponse response) {
 		
 		logger.info("Home Test");
@@ -31,7 +31,7 @@ public class cHomeCtl {
 		return "client/clientMain";
 	}
 
-	@RequestMapping(value = "/proFile", method = {RequestMethod.GET,RequestMethod.POST}, produces = "application/json; charset=UTF-8")
+	@RequestMapping(value = "/proFile", method = {RequestMethod.GET,RequestMethod.POST})
 	public String proFile(HttpServletRequest request, HttpServletResponse response){
 
 		logger.info("proFile Test");
@@ -39,7 +39,41 @@ public class cHomeCtl {
 		return "client/clientProFile";
 	}
 
+	@RequestMapping(value = "/portFolio", method = {RequestMethod.GET,RequestMethod.POST})
+	public String portFolio(HttpServletRequest request, HttpServletResponse response){
 
+		logger.info("proFile Test");
+
+		return "client/clientPortFolio";
+	}
+	@RequestMapping(value = "/game", method = {RequestMethod.GET,RequestMethod.POST})
+	public String game(HttpServletRequest request, HttpServletResponse response){
+
+		logger.info("proFile Test");
+
+		return "client/clientGame";
+	}
+	@RequestMapping(value = "/dailyLifeTip", method = {RequestMethod.GET,RequestMethod.POST})
+	public String dailyLifeTip(HttpServletRequest request, HttpServletResponse response){
+
+		logger.info("proFile Test");
+
+		return "client/clientDailyLifeTip";
+	}
+	@RequestMapping(value = "/study", method = {RequestMethod.GET,RequestMethod.POST})
+	public String study(HttpServletRequest request, HttpServletResponse response){
+
+		logger.info("proFile Test");
+
+		return "client/clientStudy";
+	}
+	@RequestMapping(value = "/setting", method = {RequestMethod.GET,RequestMethod.POST})
+	public String setting(HttpServletRequest request, HttpServletResponse response){
+
+		logger.info("proFile Test");
+
+		return "client/clientSetting";
+	}
 
 
 }
